c0307925a5633186695cc66b2c5662317b34adc0
GianbaBotassi <gianba89@gmail.com>
2023-11-07 15:13:08 +0100

    Step 2 done

--- src/main/java/com/experis/exercise/bestoftheyear/controller/IndexController.java
+++ src/main/java/com/experis/exercise/bestoftheyear/controller/IndexController.java
@@ -5,6 +5,8 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.ArrayList;
+
 ;
 
 @Controller
@@ -15,5 +17,44 @@ public class IndexController {
     public String home(Model model) {
         model.addAttribute("nome", "Gianbattista");
         return "home";
+    }
+
+    @GetMapping("movies")
+    public String movies(Model model) {
+        model.addAttribute("movies", getBestMovies());
+        return "movies";
+    }
+
+    @GetMapping("songs")
+    public String songs(Model model) {
+        model.addAttribute("songs", getBestSongs());
+        return "songs";
+    }
+
+
+    private ArrayList<String> getBestMovies() {
+        ArrayList<String> array = new ArrayList<>();
+        array.add("Ritorno al futuro");
+        array.add("Ritorno al presente");
+        array.add("Ritorno al passato");
+        array.add("Ritorno all'imperfetto");
+        array.add("Ritorno al passato remoto");
+        array.add("Ritorno al gerundio");
+        array.add("Ritorno all'infinito");
+
+        return array;
+    }
+
+    private ArrayList<String> getBestSongs() {
+        ArrayList<String> array = new ArrayList<>();
+        array.add("Bohemian Rhapsody");
+        array.add("Imagine");
+        array.add("Hey Jude");
+        array.add("Hotel California");
+        array.add("Purple Haze");
+        array.add("Like a Rolling Stone");
+        array.add("Billie Jean");
+
+        return array;
     }
 }
