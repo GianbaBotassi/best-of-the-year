8dc490f0925e099502076113c74d857f2d824fc2
GianbaBotassi <gianba89@gmail.com>
2023-11-07 15:46:44 +0100

    Refactoring, add foreach

--- src/main/java/com/experis/exercise/bestoftheyear/controller/IndexController.java
+++ src/main/java/com/experis/exercise/bestoftheyear/controller/IndexController.java
@@ -9,29 +9,33 @@ import java.util.ArrayList;
 
 ;
 
+//Annotazione Controller e request mapping su localhost
 @Controller
 @RequestMapping("/")
 public class IndexController {
 
+    //Rotta per pagina home
     @GetMapping("home")
     public String home(Model model) {
         model.addAttribute("nome", "Gianbattista");
         return "home";
     }
 
+    //Rotta pagina movies
     @GetMapping("movies")
     public String movies(Model model) {
         model.addAttribute("movies", getBestMovies());
         return "movies";
     }
 
+    //Rotta pagina songs
     @GetMapping("songs")
     public String songs(Model model) {
         model.addAttribute("songs", getBestSongs());
         return "songs";
     }
 
-
+    //Metodi che danno come output movies e songs
     private ArrayList<String> getBestMovies() {
         ArrayList<String> array = new ArrayList<>();
         array.add("Ritorno al futuro");
@@ -41,7 +45,6 @@ public class IndexController {
         array.add("Ritorno al passato remoto");
         array.add("Ritorno al gerundio");
         array.add("Ritorno all'infinito");
-
         return array;
     }
 
@@ -54,7 +57,6 @@ public class IndexController {
         array.add("Purple Haze");
         array.add("Like a Rolling Stone");
         array.add("Billie Jean");
-
         return array;
     }
 }
